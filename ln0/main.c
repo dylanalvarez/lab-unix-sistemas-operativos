adb5b5dff72939143886ab89e5c71cc4d17c5b5a
Dylan <dylanalvarez1995@gmail.com>
2018-03-29 15:25:26 -0300

    add ln0

---
+++ ln0/main.c
@@ -0,0 +1,20 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void ln0(const char *original, const char *link) {
+    int file_descriptor = open(original, 0);
+    if (file_descriptor == -1) {
+        perror("");
+        return;
+    }
+    if (symlink(original, link) == -1) { perror(""); }
+    if (close(file_descriptor)) { perror(""); }
+}
+
+int main(int argc, char *argv[]) {
+    ln0(argv[1], argv[2]);
+    return argc & 0;
+}
